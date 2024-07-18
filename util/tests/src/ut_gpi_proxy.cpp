c5a87ec0bba75a640b8f6760db3e14c710eae52a
fedddot <idanrommm@gmail.com>
2024-07-18 17:28:57 +0300

    TC added

--- util/tests/src/ut_gpi_proxy.cpp
+++ util/tests/src/ut_gpi_proxy.cpp
@@ -35,35 +35,19 @@ TEST(ut_gpi_proxy, ctor_dtor) {
 	instance_ptr = nullptr;
 }
 
-TEST(ut_gpi_proxy, run_sanity) {
-	// // GIVEN
-	// const mcu_client::ClientData test_data("MSG_HEADER{\"ctor_id\" : 0, \"gpio_dir\" : 1, \"gpio_id\" : 25}MSG_TAIL");
-	// bool data_received(false);
-	// std::mutex mux;
-	// std::condition_variable cond;
+TEST(ut_gpi_proxy, state_sanity) {
+	// GIVEN
+	UartConnection connection("/dev/ttyACM0", UartConnection::UartBaud::BAUD9600, 1000, "MSG_HEADER", "MSG_TAIL");
+	GpiProxy::McuClient client(&connection);
+	JsonDataParser parser;
+	JsonDataSerializer serializer;
 
-	// CustomListener<mcu_client::ClientData> test_listener(
-	// 	[&data_received, &mux, &cond](const mcu_client::ClientData& data) {
-	// 		std::unique_lock lock(mux);
-	// 		std::cout << "data received: " << data << std::endl;
-	// 		data_received = true;
-	// 		cond.notify_one();
-	// 		std::cout << "notified" << std::endl;
-	// 	}
-	// );
-	
-	// // WHEN
-	// Uart instance("/dev/ttyACM0", Uart::UartBaud::BAUD9600, 100);
-	// mcu_client::ClientData result("");
 
-	// // THEN
-	// ASSERT_NO_THROW(instance.start_listening(test_listener));
-	// ASSERT_TRUE(instance.is_listening());
-	
-	// std::unique_lock lock(mux);
-	// ASSERT_NO_THROW(instance.send(test_data));
-	
-	// cond.wait(lock);
-	// ASSERT_NO_THROW(instance.stop_listening());
-	// ASSERT_FALSE(instance.is_listening());
+	// WHEN
+	GpiProxy instance(25, &client, parser, serializer);
+	GpiProxy::State result(GpiProxy::State::LOW);
+
+	// THEN
+	ASSERT_NO_THROW(result = instance.state());
+	ASSERT_EQ(GpiProxy::State::HIGH, result);
 }
