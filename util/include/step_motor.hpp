28d15bb680a857b588a2f2ca184d225ccbb2cac1
fedddot <idanrommm@gmail.com>
2024-07-18 18:43:58 +0300

    implemented

--- util/include/step_motor.hpp
+++ util/include/step_motor.hpp
@@ -34,25 +34,66 @@ namespace mcu_client_utl {
 		
 		using MotorState = std::map<Shoulder, Gpo::State>;
 		using MotorStates = std::array<MotorState, MOTOR_STATES_NUMBER>;
-		using CurrentStateIter = typename MotorStates::const_iterator;
+		using StateIter = typename MotorStates::const_iterator;
 		using MotorGpos = std::map<Shoulder, Gpo *>;
 
 		MotorGpos m_gpos;
-		CurrentStateIter m_curr_state;
+		StateIter m_curr_state;
+		void apply_state(const MotorState& state);
 		static const MotorStates s_states;
 		static MotorGpos init_gpos(Gpo *lh_gpo, Gpo *ll_gpo, Gpo *rh_gpo, Gpo *rl_gpo);
+		static StateIter next_state_cw(const StateIter& curr_state);
+		static StateIter next_state_ccw(const StateIter& curr_state);
 	};
 
-
-
 	inline StepMotor::StepMotor(Gpo *lh_gpo, Gpo *ll_gpo, Gpo *rh_gpo, Gpo *rl_gpo): m_gpos(init_gpos(lh_gpo, ll_gpo, rh_gpo, rl_gpo)), m_curr_state(s_states.begin()) {
+		apply_state(*m_curr_state);
+	}
 
+	inline void StepMotor::step(const Direction& dir) {
+		StateIter next_state_iter(m_curr_state);
+		switch (dir) {
+		case Direction::CW:
+			next_state_iter = next_state_cw(m_curr_state);
+			break;
+		case Direction::CCW:
+			next_state_iter = next_state_ccw(m_curr_state);
+			break;
+		default:
+			throw std::invalid_argument("unsupported direction received");
+		}
+		apply_state(*next_state_iter);
+		m_curr_state = next_state_iter;
 	}
 
 	inline StepMotor::MotorGpos StepMotor::init_gpos(Gpo *lh_gpo, Gpo *ll_gpo, Gpo *rh_gpo, Gpo *rl_gpo) {
-
+		MotorGpos gpos {
+			{Shoulder::LH, lh_gpo},
+			{Shoulder::LL, ll_gpo},
+			{Shoulder::RH, rh_gpo},
+			{Shoulder::RL, rl_gpo}
+		};
+		for (auto item: gpos) {
+			if (!(item.second)) {
+				throw std::invalid_argument("invalid Gpo ptr received for shoulder " + std::to_string(static_cast<int>(item.first)));
+			}
+		}
+		return gpos;
 	}
 
-	
+	inline StepMotor::StateIter StepMotor::next_state_cw(const StateIter& curr_state) {
+		const StateIter next_state_iter(curr_state + 1);
+		if (s_states.end() == next_state_iter) {
+			return s_states.begin();
+		}
+		return next_state_iter;
+	}
+
+	inline StepMotor::StateIter StepMotor::next_state_ccw(const StateIter& curr_state) {
+		if (s_states.begin() == curr_state) {
+			return s_states.begin() + MOTOR_STATES_NUMBER - 1;
+		}
+		return curr_state - 1;
+	}
 }
 #endif // STEP_MOTOR_HPP
