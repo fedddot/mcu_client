4e18b57043edfca5a72050defa291d9c3274dbc1
fedddot <idanrommm@gmail.com>
2025-05-09 18:06:43 +0000

    args renamed

--- gcode_processor/src/vector_operations.rs
+++ gcode_processor/src/vector_operations.rs
@@ -1,17 +1,17 @@
 use movement_data::{Axis, Vector};
 
-pub fn add_vectors(one: &Vector<f32>, other: &Vector<f32>) -> Vector<f32> {
+pub fn add_vectors(left: &Vector<f32>, right: &Vector<f32>) -> Vector<f32> {
     let mut result = Vector::new(0.0, 0.0, 0.0);
     [Axis::X, Axis::Y, Axis::Z]
         .iter()
-        .for_each(|axis| result.set(axis, one.get(axis) + other.get(axis)));
+        .for_each(|axis| result.set(axis, left.get(axis) + right.get(axis)));
     result
 }
 
-pub fn sub_vectors(one: &Vector<f32>, other: &Vector<f32>) -> Vector<f32> {
+pub fn sub_vectors(left: &Vector<f32>, right: &Vector<f32>) -> Vector<f32> {
     let mut result = Vector::new(0.0, 0.0, 0.0);
     [Axis::X, Axis::Y, Axis::Z]
         .iter()
-        .for_each(|axis| result.set(axis, one.get(axis) - other.get(axis)));
+        .for_each(|axis| result.set(axis, left.get(axis) - right.get(axis)));
     result
 }
