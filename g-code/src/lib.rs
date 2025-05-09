708acfd8f3ec5523a23f671a63b03d96ed92b49e
fedddot <idanrommm@gmail.com>
2025-05-09 07:32:55 +0000

    partially implemented

--- g-code/src/lib.rs
+++ g-code/src/lib.rs
@@ -1,1 +1,17 @@
+pub use movement_data::Vector;
+
+#[derive(Clone)]
+pub enum Command {
+    G00, // Rapid Position
+    G01, // Linear Movement
+}
+
+#[derive(Clone)]
+pub struct GcodeData {
+    pub command: Command,
+    pub target: Option<Vector<f32>>,
+    pub rotation_center: Option<Vector<f32>>,
+    pub speed: Option<f32>,
+}
+
 mod parser;
--- g-code/src/parser.rs
+++ g-code/src/parser.rs
@@ -1,7 +1,18 @@
+use crate::{Command, GcodeData};
 
 pub struct GcodeParser;
 
 impl GcodeParser {
-    fn parse(&self, gcode_line: &str) -> MovementType
+    pub fn parse(&self, gcode_line: &str) -> GcodeData {
+        GcodeData {
+            command: self.parse_cmd(gcode_line),
+            target: todo!(),
+            rotation_center: todo!(),
+            speed: todo!(),
+        }
+    }
 
+    fn parse_cmd(&self, gcode_line: &str) -> Command {
+        todo!()
+    }
 }
