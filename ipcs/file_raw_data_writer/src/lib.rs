0c3320f14579d45040f3bbda235d003c19d3b73e
fedddot <idanrommm@gmail.com>
2025-05-24 06:08:45 +0000

    todos added

--- ipcs/file_raw_data_writer/src/lib.rs
+++ ipcs/file_raw_data_writer/src/lib.rs
@@ -4,18 +4,23 @@ pub type FileRawData = Vec<u8>;
 
 pub struct FileRawDataWriter {
     file_path: PathBuf,
+    movement_app_cli_path: PathBuf,
 }
 
 impl FileRawDataWriter {
-    pub fn new(file_path: &str) -> Self {
-        Self { file_path: PathBuf::from(file_path) }
+    pub fn new(file_path: &str, movement_app_cli_path: &str) -> Self {
+        Self {
+            file_path: PathBuf::from(file_path),
+            movement_app_cli_path: PathBuf::from(movement_app_cli_path),
+        }
     }
     
 }
 
 impl ipc::IpcWriter<FileRawData, String> for FileRawDataWriter {
     fn write_data(&mut self, data: &FileRawData) -> Result<(), String> {
-        todo!("implement write_data");
+        todo!("write the data to the file");
+        todo!("and call the movement app cli to process it");
     }
 }
 
