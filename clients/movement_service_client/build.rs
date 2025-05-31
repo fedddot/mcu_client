f2bc58ae64aa732cf905de2daae0ff140fa1cefe
fedddot <idanrommm@gmail.com>
2025-05-31 17:13:50 +0000

    deps moved, build updated

--- clients/movement_service_client/build.rs
+++ clients/movement_service_client/build.rs
@@ -1,4 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/movement_vendor_api.proto")?;
+    tonic_build::compile_protos("resources/movement_vendor_api.proto")?;
     Ok(())
 }
