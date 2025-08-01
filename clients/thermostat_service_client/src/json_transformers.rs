5b74ecc5536af96930f626fd7c6b27bd785370e1
fedddot <idanrommm@gmail.com>
2025-08-01 18:32:15 +0000

    thermostat_service_client added

---
+++ clients/thermostat_service_client/src/json_transformers.rs
@@ -0,0 +1,156 @@
+use serde_json::{json, Value};
+
+use movement_data::*;
+
+pub use crate::DataTransformer;
+
+pub struct JsonRequestSerializer;
+
+impl JsonRequestSerializer {
+    fn serialize_request_type(api_request: &MovementApiRequest) -> Value {
+        match api_request {
+            MovementApiRequest::Config { .. } => json!("CONFIG"),
+            MovementApiRequest::LinearMovement { .. } => json!("LINEAR_MOVEMENT"),
+            MovementApiRequest::RotationalMovement { .. } => json!("ROTATIONAL_MOVEMENT"),
+        }
+    }
+
+    fn serialize_request_data(api_request: &MovementApiRequest) -> Value {
+        match api_request {
+            MovementApiRequest::Config { .. } => Self::serialize_config_data(api_request),
+            MovementApiRequest::LinearMovement { .. } => Self::serialize_linear_data(api_request),
+            MovementApiRequest::RotationalMovement { .. } => Self::serialize_rotation_data(api_request),
+        }
+    }
+
+    fn serialize_config_data(data: &MovementApiRequest) -> Value {
+        let MovementApiRequest::Config { axes_configs } = data else {
+            panic!("Expected Config variant");
+        };
+        let axis_to_string = |axis: &Axis| match axis {
+            Axis::X => "x",
+            Axis::Y => "y",
+            Axis::Z => "z",
+        };
+        let mut config_data = serde_json::Value::default();
+        for (axis, config) in axes_configs {
+            let axis_config = json!(
+                {
+                    "step_length": config.step_length,
+                    "directions_mapping": config.directions_mapping,
+                    "stepper_cfg": {
+                        "enable_pin": config.stepper_config.enable_pin,
+                        "step_pin": config.stepper_config.step_pin,
+                        "dir_pin": config.stepper_config.dir_pin,
+                        "hold_time_us": config.stepper_config.hold_time_us,
+                    }
+                }
+            );
+            config_data[axis_to_string(axis)] = axis_config;
+        }
+        config_data
+    }
+
+    fn serialize_linear_data(data: &MovementApiRequest) -> Value {
+        let MovementApiRequest::LinearMovement { destination, speed } = data else {
+            panic!("Expected LinearMovement variant");
+        };
+        json!(
+            {
+                "destination": Self::serialize_vector(destination),
+                "speed": json!(speed),
+            }
+        )
+    }
+
+    fn serialize_vector(vector: &Vector<f32>) -> Value {
+        json!(
+            {
+                "x": json!(vector.get(&Axis::X)),
+                "y": json!(vector.get(&Axis::Y)),
+                "z": json!(vector.get(&Axis::Z)),
+            }
+        )
+    }
+
+    fn serialize_rotation_data(_data: &MovementApiRequest) -> Value {
+        todo!("serialize_rotation_data is not implemented yet")
+    }
+}
+
+impl DataTransformer<MovementApiRequest, Vec<u8>, String> for JsonRequestSerializer {
+    fn transform(&self, input: &MovementApiRequest) -> Result<Vec<u8>, String> {
+        let mut json_val = Self::serialize_request_data(input);
+        json_val["request_type"] = Self::serialize_request_type(input);
+        let json_string = match serde_json::to_string(&json_val) {
+            Ok(str_val) => str_val,
+            Err(err) => return Err(err.to_string()),
+        };
+        Ok(json_string.into_bytes())
+    }
+}
+
+pub struct JsonResponseParser;
+
+impl JsonResponseParser {
+    fn parse_result(json_data: &Value) -> Result<StatusCode, String> {
+        let Some(status) = json_data.get("status") else {
+            return Err("missing status field".to_string());
+        };
+        let Some(status) = status.as_str() else {
+            return Err("result field has wrong format".to_string());
+        };
+        match status {
+            "SUCCESS" => Ok(StatusCode::Success),
+            "FAILURE" => Ok(StatusCode::Error),
+            _ => Err(format!("unsupported result value: {}", status)),
+        }
+    }
+
+    fn parse_message(json_data: &Value) -> Result<Option<String>, String> {
+        let message_opt = json_data.get("message");
+        if message_opt.is_none() {
+            return Ok(None);
+        }
+        let Some(message) = message_opt.unwrap().as_str() else {
+            return Err("message field has wrong format".to_string());
+        };
+        Ok(Some(message.to_string()))
+    }
+}
+
+impl DataTransformer<Vec<u8>, MovementApiResponse, String> for JsonResponseParser {
+    fn transform(&self, input: &Vec<u8>) -> Result<MovementApiResponse, String> {
+        let json_val: Value = serde_json::from_slice(input).map_err(|err| err.to_string())?;
+        Ok(MovementApiResponse {
+            status: Self::parse_result(&json_val)?,
+            message: Self::parse_message(&json_val)?,
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use movement_data::Vector;
+
+    use super::*;
+
+    #[test]
+    fn json_request_ser_sanity() {
+        // GIVEN
+        let test_request = MovementApiRequest::LinearMovement {
+            destination: Vector::new(1.0, 2.0, 3.0),
+            speed: 4.9,
+        };
+        let mut expected_value = JsonRequestSerializer::serialize_request_data(&test_request);
+        expected_value["request_type"] = JsonRequestSerializer::serialize_request_type(&test_request);
+
+        // WHEN
+        let request_serializer = JsonRequestSerializer;
+
+        // THEN
+        let serial_request = request_serializer.transform(&test_request).unwrap();
+        let parsed_serial_request: Value = serde_json::from_slice(&serial_request).unwrap();
+        assert_eq!(expected_value, parsed_serial_request);
+    }
+}
---
+++ clients/thermostat_service_client/src/lib.rs
@@ -0,0 +1,133 @@
+use ipc::{IpcReader, IpcWriter};
+
+pub use client::ServiceClient;
+pub use movement_data::{MovementApiRequest, MovementApiResponse};
+
+pub type RequestSerializer = dyn DataTransformer<MovementApiRequest, Vec<u8>, String>;
+pub type ResponseParser = dyn DataTransformer<Vec<u8>, MovementApiResponse, String>;
+pub type RawDataReader = dyn IpcReader<Vec<u8>, String>;
+pub type RawDataWriter = dyn IpcWriter<Vec<u8>, String>;
+
+pub use json_transformers::{JsonRequestSerializer, JsonResponseParser};
+pub use proto_transformers::{ProtoRequestSerializer, ProtoResponseParser};
+
+pub struct MovementServiceClient {
+    raw_data_reader:        Box<RawDataReader>,
+    raw_data_writer:        Box<RawDataWriter>,
+    request_serializer:     Box<RequestSerializer>,
+    response_parser:        Box<ResponseParser>,
+}
+
+impl MovementServiceClient {
+    pub fn new(
+        raw_data_reader:        Box<RawDataReader>,
+        raw_data_writer:        Box<RawDataWriter>,
+        request_serializer:     Box<RequestSerializer>,
+        response_parser:        Box<ResponseParser>,
+    ) -> Self {
+        Self {
+            raw_data_reader,
+            raw_data_writer,
+            request_serializer,
+            response_parser,
+        }
+    }
+}
+
+impl ServiceClient<MovementApiRequest, MovementApiResponse, String> for MovementServiceClient {
+    fn run_request(&mut self, request: &MovementApiRequest) -> Result<MovementApiResponse, String> {
+        let serial_request = self.request_serializer.transform(request)?;
+        self.raw_data_writer.write_data(&serial_request)?;
+        let serial_response = self.raw_data_reader.read_data()?;
+        let response = self.response_parser.transform(&serial_response)?;
+        Ok(response)
+    }
+}
+
+pub trait DataTransformer<Input, Output, Error> {
+    fn transform(&self, input: &Input) -> Result<Output, Error>;
+}
+
+mod json_transformers;
+mod proto_transformers;
+
+#[cfg(test)]
+mod test {
+    use std::collections::HashMap;
+
+    use super::*;
+    use mockall::mock;
+    use serde_json::json;
+    use movement_data::Vector;
+
+    #[test]
+    fn client_new_sanity() {
+        // GIVEN
+        let test_raw_data_reader = MockIpcReader::default();
+        let test_raw_data_writer = MockIpcWriter::default();
+
+        // THEN
+        let _ = MovementServiceClient::new(
+            Box::new(test_raw_data_reader),
+            Box::new(test_raw_data_writer),
+            Box::new(JsonRequestSerializer),
+            Box::new(JsonResponseParser),
+        );
+    }
+
+    #[test]
+    fn client_run_request_sanity() {
+        // GIVEN
+        let test_config_req = MovementApiRequest::Config {
+            axes_configs: HashMap::new(),
+        };
+        let test_linear_mvmnt_req = MovementApiRequest::LinearMovement {
+            destination: Vector::new(1.0, 2.0, 3.0),
+            speed: 4.0,
+        };
+        let mut test_raw_data_reader = MockIpcReader::default();
+        test_raw_data_reader
+            .expect_read_data()
+            .returning(move || {
+                let json_response = json!({
+                    "result": "SUCCESS",
+                });
+                let serial_response = serde_json::to_vec(&json_response).unwrap();
+                Ok(serial_response)
+            });
+        let mut test_raw_data_writer = MockIpcWriter::default();
+        test_raw_data_writer
+            .expect_write_data()
+            .returning(|data| {         
+                println!("Writing data: {:?}", std::str::from_utf8(data).unwrap());
+                Ok(())
+            });
+        // WHEN
+        let mut client = MovementServiceClient::new(
+            Box::new(test_raw_data_reader),
+            Box::new(test_raw_data_writer),
+            Box::new(JsonRequestSerializer),
+            Box::new(JsonResponseParser),
+        );
+
+        // THEN
+        let response = client.run_request(&test_linear_mvmnt_req);
+        assert!(response.is_ok());
+
+        let response = client.run_request(&test_config_req);
+        assert!(response.is_ok());
+    }
+
+    mock! {
+        pub IpcReader {}
+        impl IpcReader<Vec<u8>, String> for IpcReader {
+            fn read_data(&mut self) -> Result<Vec<u8>, String>;
+        }
+    }
+    mock! {
+        pub IpcWriter {}
+        impl IpcWriter<Vec<u8>, String> for IpcWriter {
+            fn write_data(&mut self, data: &Vec<u8>) -> Result<(), String>;
+        }
+    }
+}
