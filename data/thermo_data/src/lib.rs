5d064d169e91e51db1eedada9a4e45b5e04e7bca
fedddot <idanrommm@gmail.com>
2025-08-02 11:42:59 +0000

    serde added

--- data/thermo_data/src/lib.rs
+++ data/thermo_data/src/lib.rs
@@ -1,25 +1,27 @@
-#[derive(Clone, Debug)]
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct ThermostatApiRequest {
     pub request_type: RequestType,
 	pub set_temperature: Option<f32>,
     pub time_resolution_ms: Option<u32>,
 }
 
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub enum RequestType {
     Start,
     Stop,
     GetTemperature,
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct ThermostatApiResponse {
     pub status: StatusCode,
     pub message: Option<String>,
     pub current_temperature: Option<f32>,
 }
 
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub enum StatusCode {
     Success,
     Failure,
