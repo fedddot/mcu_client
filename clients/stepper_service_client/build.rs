da22b41aae4ff27ff5859fe1d92d26ea7c1fead9
fedddot <idanrommm@gmail.com>
2025-09-24 17:55:32 +0000

    proto updated

--- clients/stepper_service_client/build.rs
+++ clients/stepper_service_client/build.rs
@@ -1,4 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("resources/service_api.proto")?;
+    tonic_build::compile_protos("resources/stepper_service.proto")?;
     Ok(())
 }
